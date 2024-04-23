dace1920da7b6edcb3fc86e458466ae38c3ca242
ozXoz <honourkorkmaz@gmail.com>
2024-04-22 21:35:25 -0400

    implement login component

--- frontend/src/components/LoginForm.tsx
+++ frontend/src/components/LoginForm.tsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react'; // Import useState from react
 import { useNavigate } from 'react-router-dom'; // Import useNavigate from react-router-dom
 import { login } from '../services/apiService'; // Adjust the import path as necessary to where login is defined
 import '../css/LoginForm.css'; // Import the CSS file
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const LoginForm: React.FC = () => {
   const [email, setEmail] = useState<string>(''); // Use useState for managing email and password
@@ -14,11 +16,13 @@ const LoginForm: React.FC = () => {
       if (response && response.token) {
         navigate('/products'); // Navigate to products page on successful login
       } else {
-        alert('Login unsuccessful, please check your credentials.');
+        // alert('Login unsuccessful, please check your credentials.');
+        toast.error('Login unsuccessful, please check your credentials.');
       }
     } catch (error: any) { // Using any here for simplicity, consider defining a more specific error type
       console.error('Login failed:', error);
-      alert('Failed to login: ' + error.message);
+      toast.error('Invalid email or password, please try again.');
+      // alert('Failed to login: ' + error.message);
     }
   };
 
@@ -29,6 +33,8 @@ const LoginForm: React.FC = () => {
         <input type="email" value={email} onChange={e => setEmail(e.target.value)} placeholder="Enter your email" />
         <input type="password" value={password} onChange={e => setPassword(e.target.value)} placeholder="Enter your password" />
         <button onClick={handleLogin}>Login</button>
+        <ToastContainer position="top-right" autoClose={5000} hideProgressBar newestOnTop closeOnClick pauseOnHover draggable />
+
       </div>
     </div>
   );
